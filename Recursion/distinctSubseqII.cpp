f87946a004f686772232515b5e0b26176ca26b9b
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2025-12-20 22:35:18 +0530

    Update distinctSubseqII.cpp
    solve it using this recurrsive approach

--- Recursion/distinctSubseqII.cpp
+++ Recursion/distinctSubseqII.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    void DistinctSubsequences(string str,int index,int n,set<string> &out,string list)
+    {
+        if(index>=n){
+            out.insert(list);
+            return;
+        } 
+        // for taking a sequence call;
+        list = list + str[index];
+        DistinctSubsequences(str,index+1,n,out,list);
+        //for not taking sequence call
+        list.pop_back();
+        DistinctSubsequences(str,index+1,n,out,list);
+    }
+    int distinctSubseqII(string s) {
+        int n = s.size();
+        set<string> out;
+        string list = "";
+        DistinctSubsequences(s,0,n,out,list);
+        return out.size() - 1;
+    }
+};
