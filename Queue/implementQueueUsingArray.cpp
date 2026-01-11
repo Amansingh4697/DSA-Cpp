4f12639ffe1e24032fa7e7e4e27f7da16dd0d5be
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-01-11 22:11:45 +0530

    Update implementQueueUsingArray.cpp

--- Queue/implementQueueUsingArray.cpp
+++ Queue/implementQueueUsingArray.cpp
@@ -60,3 +60,62 @@ cout<<qe->size()<<" ";
 qe->push(110);
 cout<<qe->top()<<" ";
 }
+
+gfg
+
+class myStack {
+  public:
+   
+    int* arr;
+    int capacity;
+    int top;
+    myStack(int n) {
+        // Define Data Structures
+        capacity = n;
+        arr = new int[capacity];
+        top = -1;
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        if(top == -1) return true;
+        return false;
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        if(top >= capacity-1) return true;
+        return false;
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+        // if(top>=capacity-1){
+        //     cout<<"stack overflow";
+        //     return;
+        // }
+        if(isFull()){
+            return;
+        }
+        top = top + 1;
+        arr[top] = x;
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+        // if(top == -1){
+        //     cout<<"stack Underflow";
+        //     return;
+        // }
+        if(isEmpty()){
+            return;
+        }
+        top = top-1;
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        if(top == -1) return -1;
+        return arr[top];
+    }
+};
