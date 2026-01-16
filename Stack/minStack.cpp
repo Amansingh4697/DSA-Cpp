67adf331d324ef4d403d7f1400bd0e68110da084
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-01-16 21:40:13 +0530

    Update minStack.cpp

--- Stack/minStack.cpp
+++ Stack/minStack.cpp
@@ -36,3 +36,60 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+// Approach2
+class MinStack {
+public:
+   stack<long long> st;
+   long long  min = INT_MAX;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push(val);
+            min = val;
+        }
+        else{
+            if(val<min){
+                st.push(1LL*2*val-min);
+                min = val;
+            }
+            else{
+                st.push(val);
+            }
+        }
+    }
+    
+    void pop() {
+        if(st.empty()) return;
+        long long x = st.top();
+        st.pop();
+        if(x<min){
+            min = 2*min-x;
+        }
+    }
+    
+    int top() {
+        if(st.empty()) return -1;
+        long long x = st.top();
+        if(x<min){
+          return min;
+        }
+        return x;
+    }
+    
+    int getMin() {
+        if(st.empty()) return -1;
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
