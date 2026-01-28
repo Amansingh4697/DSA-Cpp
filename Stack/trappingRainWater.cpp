c47bc467e903544c68bd3239a11b2bf8af215f2c
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-01-28 21:37:42 +0530

    Update trappingRainWater.cpp

--- Stack/trappingRainWater.cpp
+++ Stack/trappingRainWater.cpp
@@ -5,16 +5,17 @@ public:
         int total = 0;
         vector<int> prefixMax(n);
         vector<int> suffixMax(n);
-        prefixMax[0] = height[0];
-        for(int i = 1;i<n;i++){
-            prefixMax[i] = max(prefixMax[i-1],height[i]);
-        }
+        // prefixMax[0] = height[0];
+        // for(int i = 1;i<n;i++){
+        //     prefixMax[i] = max(prefixMax[i-1],height[i]);
+        // }
         suffixMax[n-1] = height[n-1];
          for(int i = n-2;i>=0;i--){
             suffixMax[i] = max(suffixMax[i+1],height[i]);
         }
+        int leftMax = height[0];
         for(int i = 0;i<n;i++){
-            int leftMax = prefixMax[i];
+            leftMax = max(leftMax,height[i]); 
             int rightMax = suffixMax[i];
             if(height[i]<leftMax && height[i]<rightMax);{
                 total += min(leftMax,rightMax) - height[i];
