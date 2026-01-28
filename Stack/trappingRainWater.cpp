bf5c9772febcc563408e2bd30a04c558f6b15b26
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-01-28 22:22:48 +0530

    Update trappingRainWater.cpp

--- Stack/trappingRainWater.cpp
+++ Stack/trappingRainWater.cpp
@@ -24,3 +24,36 @@ public:
         return total;
     }
 };
+// travel lowest first (optimization)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0;
+        int r = n-1;
+        int leftMax = height[l];
+        int rightMax = height[r];
+        int total = 0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(leftMax>height[l]){
+                  total += (leftMax - height[l]);
+                }else{
+                    leftMax = height[l];
+                }
+                 l = l+1;
+            }
+            else{
+                if(rightMax>height[r]){
+                    total += (rightMax - height[r]);
+                }
+                else{
+                    rightMax = height[r];
+                }
+                r = r-1;
+            }
+        }
+        return total;
+
+    }
+};
