5008621e4ff1722ccec3fb05c5168e36614889ca
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-03-03 21:24:05 +0530

    Implement sliding window for fruit basket problem

--- SlidingWindow/fruitsintoBasket.cpp
+++ SlidingWindow/fruitsintoBasket.cpp
@@ -21,3 +21,46 @@ public:
         return maxFruits;
     }
 };
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        // int left = 0;
+        // int right  = 0;
+        // map<int,int> mpp;
+        // int maxFruits = 0;
+        // while(right<fruits.size()){
+        //      mpp[fruits[right]]++;
+        //     while(mpp.size() > 2){
+        //         mpp[fruits[left]]--;
+        //         if(mpp[fruits[left]] == 0){
+        //             mpp.erase(fruits[left]);
+        //         }
+        //         left++;
+        //     }
+        //     int cntFruits = right-left+1;
+        //     maxFruits = max(maxFruits,cntFruits);
+        //     right++;
+        // }
+        // return maxFruits;
+         int left = 0;
+        int right  = 0;
+        map<int,int> mpp;
+        int maxFruits = 0;
+        while(right<fruits.size()){
+            mpp[fruits[right]]++;
+            if(mpp.size()>2){
+                mpp[fruits[left]]--;
+                if(mpp[fruits[left]] == 0){
+                    mpp.erase(fruits[left]);
+                }
+                left++;
+            }
+            if(mpp.size()<=2){
+                int cnt = right-left+1;
+                maxFruits = max(cnt,maxFruits);
+            }
+            right++;
+        }
+        return maxFruits;
+    }
+};
