bf17ed4b4bfe6fb06edda29fcf123b49cbaab352
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-03-07 11:53:47 +0530

    Add function for longest substring with K distinct chars
    Implement function to find longest substring with at most K distinct characters.

---
+++ SlidingWindow/Longest Substring with At Most K Distinct Characters.cpp
@@ -0,0 +1,28 @@
+#include <map>
+int kDistinctChars(int k, string &str)
+{
+    // Write your code here
+    int maxLen = -1;
+    int l = 0;
+    int r = 0;
+    int n = str.size();
+    map<char,int> mpp;
+    while(r<n){
+        mpp[str[r]]++;
+        if(mpp.size()>k){
+            mpp[str[l]]--;
+            if(mpp[str[l]] == 0){
+                mpp.erase(str[l]);
+            }
+            l++;
+        }
+        int len = (r-l+1);
+        if(mpp.size() <= k) {
+          maxLen = max(maxLen,len);
+        }
+        r++;
+    }
+    return maxLen;
+}
+
+
