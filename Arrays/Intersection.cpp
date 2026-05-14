c11969f6d3324e2587560fb884458aaf391a1705
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-05-14 15:40:34 +0530

    Update Intersection.cpp

--- Arrays/Intersection.cpp
+++ Arrays/Intersection.cpp
@@ -15,15 +15,34 @@ public:
         //     }
         // }
         // return res;
-        map<int,int> freq;
+        // map<int,int> freq;
+        // vector<int> res;
+        // for(int i  = 0;i<nums1.size();i++){
+        //     freq[nums1[i]]++;
+        // }
+        // for(auto it:nums2){
+        //     if(freq[it]>0){
+        //         res.push_back(it);
+        //         freq[it]--;
+        //     }
+        // }
+        // return res;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
         vector<int> res;
-        for(int i  = 0;i<nums1.size();i++){
-            freq[nums1[i]]++;
-        }
-        for(auto it:nums2){
-            if(freq[it]>0){
-                res.push_back(it);
-                freq[it]--;
+        int i = 0;
+        int j = 0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] ==  nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
             }
         }
         return res;
