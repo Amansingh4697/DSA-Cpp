c914cde3c75cfd7c43b6dddfdda4be55209a392f
Aman Singh <162338337+Amansingh4697@users.noreply.github.com>
2026-05-22 11:08:47 +0530

    Add frequency counting function in frequencyofElement.cpp
    Implement countFreq function to count frequency of elements in an array.

---
+++ Arrays/frequencyofElement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<vector<int>> countFreq(vector<int>& arr) {
+        // code here
+        unordered_map<int,int> mpp;
+         vector<vector<int>> ans;
+        for(int i = 0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        for(auto it:mpp){
+            ans.push_back({it.first,it.second});
+        }
+        return ans;
+    }
+};
